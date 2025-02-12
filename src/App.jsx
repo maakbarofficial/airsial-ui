614981921ebcc8fe5a92feed5af1cefaf6e13128
maakbarofficial <maakbarofficial@gmail.com>
2025-02-12 19:22:39 +0500

    Add necessary packages, vercel config, routing and not found view

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { ErrorBoundary } from "react-error-boundary"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import NotFound from "./views/NotFound/NotFound"
+
 function App() {
   return (
-    <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <BrowserRouter>
+      <ErrorBoundary>
+        <Routes>
+          <Route path="/" element={<h1>AirSial UI</h1>} />
+          <Route path="/about" element={<h1>About AirSial UI</h1>} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </ErrorBoundary>
+    </BrowserRouter >
   )
 }
 
